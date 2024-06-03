1e82422bfb9e2ff56f4af5edd4c94c1bb79df56b
SlothDevAsh <sloth.dev18@gmail.com>
2024-06-04 03:01:16 +0500

    Fixed icon imports

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,6 +1,3 @@
-const BASE_URL = '@assets/images/';
-
-const SEARCH_ICON = require(`${BASE_URL}search.png`);
-
-const SORT_ICON = require(`${BASE_URL}sort.png`);
+const SEARCH_ICON = require('@assets/images/search.png');
+const SORT_ICON = require('@assets/images/sort.png');
 export {SEARCH_ICON, SORT_ICON};
